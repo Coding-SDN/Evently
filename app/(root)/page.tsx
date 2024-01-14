d3b3dddf92716f246bd2cac471a56a123f430ce5
Steeve's MacBook Air <user@Steeve-MacBook-Air.local>
2024-01-14 16:29:25 +0100

    first commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,31 @@
+import CategoryFilter from '@/components/shared/CategoryFilter';
+import Collection from '@/components/shared/Collection'
+import Search from '@/components/shared/Search';
 import { Button } from '@/components/ui/button'
+import { getAllEvents } from '@/lib/actions/event.actions';
+import { SearchParamProps } from '@/types';
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
+
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6
+  })
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Host, Connect, Celebrate: <span className='text-primary-500'>Your Events, Our Platform!</span></h1>
-            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 13,438+ mentors in world-class companies with our global community.</p>
+            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
+            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
                 Explore Now
@@ -31,11 +47,11 @@ export default function Home() {
         <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          Search
-          CategoryFilter
+          <Search />
+          <CategoryFilter />
         </div>
 
-        {/* <Collection 
+        <Collection 
           data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
@@ -43,9 +59,8 @@ export default function Home() {
           limit={6}
           page={page}
           totalPages={events?.totalPages}
-        /> */}
+        />
       </section>
-
     </>
   )
 }
