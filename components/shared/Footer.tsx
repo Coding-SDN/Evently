cbd01e487648d98e4538beb51424b2c27d98c9fc
Steeve's MacBook Air <user@Steeve-MacBook-Air.local>
2024-01-17 20:44:08 +0100

    first commit

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
           />
         </Link>
 
-        <p>2022 - {new Date().getFullYear()} Evently. All Rights Reserved &copy;.</p>
+        <p>*MatTizx-IG* 2022 - {new Date().getFullYear()} Evently. All Rights Reserved &copy;.</p>
       </div>
     </footer>
   )
