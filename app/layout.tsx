6173fa4cbbea8c7ccdd59e0c0bf35048c1f79911
Steeve's MacBook Air <user@Steeve-MacBook-Air.local>
2024-01-20 07:56:51 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,37 @@
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 
-import './globals.css'
+import "./globals.css";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-poppins',
-})
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
-  title: 'Evently | Host, Connect, Celebrate',
-  description: 'Evently is a platform for event management.',
+  title: "Evently | Host, Connect, Celebrate",
+  description: "Evently is a platform for event management.",
   icons: {
-    icon: '/assets/images/logo.svg'
-  }
-}
+    icon: "/assets/images/logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider> 
+    <ClerkProvider>
       <html lang="en">
-        <body className={poppins.variable}>{children}</body>
+        <body className={poppins.variable}>
+          {children}
+          <Analytics />
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
