4203aef9b14866b54e124e0dff2a481e2d82b933
Steeve's MacBook Air <user@Steeves-MacBook-Air.local>
2024-01-27 12:26:10 +0100

    first commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,7 +24,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Host, Connect, Celebrate: <span className="text-primary-500">Your Events, Our Platform!</span></h1>
+            <h1 className="h1-bold">Host, Connect, Celebrate: <span className="text-primary-500">Your Occasions, Our Stage!</span></h1>
             <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 13M+ mentors in world-class companies with our global community.</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
