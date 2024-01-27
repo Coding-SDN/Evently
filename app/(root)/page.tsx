a324e3e9c3ac9eb8828aed69e7ce159efb267704
Steeve's MacBook Air <user@Steeves-MacBook-Air.local>
2024-01-27 12:38:52 +0100

    first commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,8 +24,8 @@ export default async function Home({ searchParams }: SearchParamProps) {
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Welcome, Join, Commemorate: <span className="text-primary-500">Ur Occasions, Our Stage!</span></h1>
-            <p className="p-regular-20 md:p-regular-24">Reserve and acquire valuable insights from over 13M+ mentors in top-tier organizations through our worldwide network.</p>
+            <h1 className="h1-bold">Host, Connect, Celebrate: <span className="text-primary-500">Your Events, Our Platform!</span></h1>
+            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 13M+ mentors in world-class companies with our global community.</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
                 Explore Now
@@ -44,7 +44,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
       </section> 
 
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Reliability through <br /> Numerous Events</h2>
+        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
